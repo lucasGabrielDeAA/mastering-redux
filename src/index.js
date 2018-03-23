5884354b402c6988b62c292bf9cdc54832f5eb3d
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-23 18:10:36 -0300

    REfactoring app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 
 import 'config/ReactotronConfig';
 import store from 'store';
 
-import TodoList from './TodoList';
+//import TodoList from './TodoList';
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <View />
   </Provider>
 );
 
