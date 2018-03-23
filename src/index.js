5d59b79095eb71f336f13392f958be253a75ad7d
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-23 18:18:05 -0300

    Routes file added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 
 import 'config/ReactotronConfig';
-import store from 'store';
 
-//import TodoList from './TodoList';
+import store from 'store';
+import Routes from './routes';
 
 const App = () => (
   <Provider store={store}>
-    <View />
+    <Routes />
   </Provider>
 );
 
