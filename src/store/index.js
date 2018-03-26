290402941465a49b588f7507dad98d39c25cd4ab
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-26 15:18:46 -0300

    Saga configuration added

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducers from './reducers';
+import sagas from './sagas';
 
-const middleware = [];
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [
+  sagaMiddleware,
+];
 
 const createApropriateStore = __DEV__ ? console.tron.createStore : createStore;
 const store = createApropriateStore(reducers, applyMiddleware(...middleware));
+
+sagaMiddleware.run(sagas);
 
 export default store;
