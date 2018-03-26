3399aaf4648600892c41c35fd2f9b499b97771f7
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-26 14:50:13 -0300

    Styling FavoriteItem's component

--- src/pages/favorites/components/FavoriteItem/index.js
+++ src/pages/favorites/components/FavoriteItem/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import {
   View,
   Text,
+  Image,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
-const FavoriteItem = ({ repository }) => (
-  <View>
-    <Text>{repository.full_name}</Text>
+import styles from './styles';
+
+const FavoriteItem = ({ repository: { name, owner, description } }) => (
+  <View style={styles.container}>
+    <Image style={styles.avatar} source={{ uri: owner.avatar_url }} />
+    <View style={styles.info}>
+      <Text style={styles.title}>{name}</Text>
+      <Text numberOfLines={2} style={styles.description}>{description}</Text>
+    </View>
   </View>
 );
+
+FavoriteItem.propTypes = {
+  repository: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    owner: PropTypes.shape({
+      avatar_url: PropTypes.string,
+    }).isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default FavoriteItem;
