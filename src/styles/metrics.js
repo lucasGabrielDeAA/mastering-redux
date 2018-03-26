0ef8144ed15d704e46ba01fb7822495c3d686dbe
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-26 14:49:43 -0300

    Bugfix metrics

--- src/styles/metrics.js
+++ src/styles/metrics.js
@@ -3,8 +3,8 @@ import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 export default {
-  basePAdding: 20,
-  baseMArgin: 10,
+  basePadding: 20,
+  baseMargin: 10,
   baseRadius: 3,
   screenWidth: width < height ? width : height,
   screeHeight: width < height ? height : width,
