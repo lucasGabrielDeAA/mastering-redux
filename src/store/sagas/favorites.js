0a467ed582068838eb8d1c3fb28f33053e5dbc88
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-27 14:37:28 -0300

    Validation do duplicate repository added

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 
 import api from 'services/api';
 
@@ -10,7 +10,14 @@ import {
 export function* addFavoriteRequest(action) {
   try {
     const response = yield call(api.get, `/repos/${action.payload.repositoryName}`);
-    yield put(addFavoriteSuccess(response.data));
+
+    const favorites = yield select(state => state.favorites.data);
+
+    if (favorites.find(favorite => favorite.id === response.data.id)) {
+      yield put(addFavoriteError('Repositório já adicionado'));
+    } else {
+      yield put(addFavoriteSuccess(response.data));
+    }
   } catch (error) {
     yield put(addFavoriteError('Repositório não encontrado'));
   }
